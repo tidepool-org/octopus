f424ae830c83149140d278d01494173971bb3178
Jamie <jamie@tidepool.org>
2014-12-29 11:55:23 +1300

    start of cli for testing purposed

---
+++ cli.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+func main() {
+
+	const (
+		DEFAULT_QUERY = "METAQUERY WHERE userid IS %s QUERY TYPE IN cbg, smbg SORT BY time AS Timestamp REVERSED"
+	)
+
+	who := flag.String("w", "", "who we are fetching data for")
+	query := flag.String("q", DEFAULT_QUERY, "the query to execute")
+	usr := flag.String("u", "", "tidepool username")
+	pw := flag.String("p", "", "tidepool password")
+
+	flag.Parse()
+
+	log.Printf("for [%s] run [%s] logged in as [%s]", *who, fmt.Sprintf(*query, *who), *usr)
+
+}
