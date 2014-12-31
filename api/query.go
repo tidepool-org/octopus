edcaf7aff3b837cae6bec7d0a2103872af14e8b7
Jamie <jamie@tidepool.org>
2014-12-31 15:20:06 +1300

    ensure user is authorised

--- api/query.go
+++ api/query.go
@@ -13,41 +13,41 @@ import (
 // http.StatusUnauthorized
 func (a *Api) Query(res http.ResponseWriter, req *http.Request) {
 
-	//if a.authorized(req) {
+	if a.authorized(req) {
 
-	log.Print("Query: starting ... ")
+		log.Print("Query: starting ... ")
 
-	defer req.Body.Close()
-	if rawQuery, err := ioutil.ReadAll(req.Body); err != nil || string(rawQuery) == "" {
-		log.Printf("Query: err decoding nonempty response body: [%v]\n [%v]\n", err, req.Body)
-		res.WriteHeader(http.StatusBadRequest)
-		return
-	} else {
-		query := string(rawQuery)
-
-		log.Printf("Query: to execute [%s] ", query)
-
-		if errs, qd := model.ExtractQuery(query); len(errs) != 0 {
-
-			log.Printf("Query: errors [%v] found parsing raw query [%s]", errs, query)
+		defer req.Body.Close()
+		if rawQuery, err := ioutil.ReadAll(req.Body); err != nil || string(rawQuery) == "" {
+			log.Printf("Query: err decoding nonempty response body: [%v]\n [%v]\n", err, req.Body)
 			res.WriteHeader(http.StatusBadRequest)
 			return
-
 		} else {
+			query := string(rawQuery)
 
-			log.Printf("Query: data used [%v]", qd)
+			log.Printf("Query: to execute [%s] ", query)
 
-			result := a.Store.ExecuteQuery(qd)
+			if errs, qd := model.ExtractQuery(query); len(errs) != 0 {
 
-			log.Printf("Query: results [%s]", string(result))
+				log.Printf("Query: errors [%v] found parsing raw query [%s]", errs, query)
+				res.WriteHeader(http.StatusBadRequest)
+				return
 
-			res.WriteHeader(http.StatusOK)
-			res.Write(result)
-			return
+			} else {
+
+				log.Printf("Query: data used [%v]", qd)
+
+				result := a.Store.ExecuteQuery(qd)
+
+				log.Printf("Query: results [%s]", string(result))
+
+				res.WriteHeader(http.StatusOK)
+				res.Write(result)
+				return
+			}
 		}
 	}
-	//}
-	//log.Print("Query: failed authorization")
-	//res.WriteHeader(http.StatusUnauthorized)
-	//return
+	log.Print("Query: failed authorization")
+	res.WriteHeader(http.StatusUnauthorized)
+	return
 }
